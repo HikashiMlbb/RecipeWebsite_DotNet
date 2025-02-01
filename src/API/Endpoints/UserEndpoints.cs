0e36cdbf8dafe57fe4ee0b540d7b737e59af47f5
hikashi <hikashimlbb@gmail.com>
2025-02-01 13:28:27 +0700

    Added User Login Endpoint

--- src/API/Endpoints/UserEndpoints.cs
+++ src/API/Endpoints/UserEndpoints.cs
@@ -1,3 +1,7 @@
+using Application.Users.UseCases;
+using Application.Users.UseCases.Login;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Endpoints;
 
 public static class UserEndpoints
@@ -6,9 +10,18 @@ public static class UserEndpoints
     {
         var route = app.MapGroup(path).WithTags("User Endpoints:");
 
-        route.MapPost("/login", () => "Login.");
+        route.MapPost("/login", Login);
         route.MapPost("/signup", () => "Sign up.");
         route.MapPut("/", () => "Change username and password.");
         route.MapGet("/{id:int}", () => "Get by ID.");
+    }
+
+    private static async Task<IResult> Login([FromBody]UserDto dto, [FromServices]UserLogin userLogin)
+    {
+        var loginResult = await userLogin.LoginAsync(dto);
+
+        return loginResult.IsSuccess
+            ? Results.Ok(loginResult.Value!)
+            : Results.Problem(statusCode: 401, title: loginResult.Error!.Code, detail: loginResult.Error.Description);
     }
 }
